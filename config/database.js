250d4a9c7275a3e7377f8433a2b425c1cf1e3583
Sunilkumar <ksunilkumar1123@gmail.com>
2026-04-14 09:07:29 +0530

    fix: removed nodemon for production

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 const connectDatabase= async ()=>{
 
     try {
-        const con = await mongoose.connect(process.env.DB_LOCAL_URI);
+        const con = await mongoose.connect(process.env.DB_MONGO_URI);
         console.log(`✅ MongoDB connected: ${con.connection.host}`);
     } catch (err) {
         console.error(" MongoDB connection error:", err.message);
