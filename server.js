9a9c0381311e1258595a1e7a6fb925ac45c71a91
Sunilkumar <ksunilkumar1123@gmail.com>
2026-08-20 21:52:38 +0530

    fix: bind server to render port

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.use('/admin', adminRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
 
     console.log(`Server running on http://localhost:${PORT}`)
 });
